491ee3702db7831ca5f008df1b11b5b86b8c3773
Carlos Henrique <carloshribeiro99@gmail.com>
2025-01-02 18:10:17 -0300

    components dinamycs propertys

--- src/app/components/menu-title/menu-title.component.ts
+++ src/app/components/menu-title/menu-title.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu-title',
   standalone: true,
-  imports: [],
+  imports:[],
   templateUrl: './menu-title.component.html',
-  styleUrl: './menu-title.component.css'
+  styleUrl: './menu-title.component.css',
+
+
 })
 export class MenuTitleComponent {
 
